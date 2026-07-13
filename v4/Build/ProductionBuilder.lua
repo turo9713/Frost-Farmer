979c7953d0dd301d33b3385a42364e9be32763a8
turo9713 <turo9713@mail.ru>
2026-07-13 18:53:09 +0300

    Add v4 production builder

---
+++ v4/Build/ProductionBuilder.lua
@@ -0,0 +1,11 @@
+-- Frost-Farmer v4 Production Builder
+local Builder = {}
+
+function Builder.build()
+    return {
+        status = "BUILD_READY",
+        version = "4.0"
+    }
+end
+
+return Builder
