689a610d925f663faae85450bf7c8390373ff141
turo9713 <turo9713@mail.ru>
2026-07-13 13:01:04 +0300

    Commit #0018 Add AI Assistant

---
+++ AI/Assistant.lua
@@ -0,0 +1,9 @@
+local Assistant = {}
+FrostFarmer.AI = FrostFarmer.AI or {}
+FrostFarmer.AI.Assistant = Assistant
+
+function Assistant:Suggest()
+    return "Analyzing next action"
+end
+
+return Assistant
