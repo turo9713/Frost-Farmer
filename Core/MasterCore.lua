e4d3f9b8c73cb4ad5b93c0498b34e603fa892107
turo9713 <turo9713@mail.ru>
2026-07-13 18:16:22 +0300

    Add Master Core prototype module

---
+++ Core/MasterCore.lua
@@ -0,0 +1,12 @@
+-- Frost-Farmer Master Core
+local MasterCore = {}
+
+function MasterCore.status()
+  return "Frost-Farmer Prototype Online"
+end
+
+function MasterCore.start()
+  return "Core initialized"
+end
+
+return MasterCore
