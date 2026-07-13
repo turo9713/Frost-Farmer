af6f108adfcd719e1a296d921babbe426aabaf36
turo9713 <turo9713@mail.ru>
2026-07-13 17:50:14 +0300

    Extend adaptive learning module

--- Learning/AdaptiveLearning.lua
+++ Learning/AdaptiveLearning.lua
@@ -5,4 +5,8 @@ function AdaptiveLearning.update(experience)
     return {learned = true, source = experience}
 end
 
+function AdaptiveLearning.score(result)
+    return {score = result and 1 or 0}
+end
+
 return AdaptiveLearning
